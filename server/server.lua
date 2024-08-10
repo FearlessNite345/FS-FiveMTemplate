c4db5d841f9e33c4185fda5e77993c81967040ec
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2024-08-10 02:38:21 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,3 @@
 -- Main server code goes here
 
-exports['FS-Lib']:VersionCheck('FS-FiveMTemplate', 'fearlessnite345/fs-fivemtemplate')
+exports['FS-Lib']:VersionCheck('resource-display-name', 'username/repo')
